19453a4537df8e720a25a068c6daeb224a48eac1
System46 <60218442+System46@users.noreply.github.com>
2020-01-24 15:51:54 +0100

    Update smm1patcher.py

--- smm1patcher/smm1patcher.py
+++ smm1patcher/smm1patcher.py
@@ -71,3 +71,14 @@ def change_camera_zoom(camera_zoom):
                         smm1data[0x583830:0x583834] = b'\x7c\x03\x18\x00'
                         with open(main_binary,'wb') as smm1binary:
                             smm1binary.write(smm1data)
+def change_mario_run_speed_right_only(speed_float):
+    global main
+    with open(main_binary,'rb') as smm1binary:
+            smm1data = smm1binary.read()
+            smm1data = bytearray(smm1data)
+            speed_float = hex(struct.unpack('<I', struct.pack('<f', speed_float))[0])
+            speed_float = bytes.fromhex(str(speed_float).replace("0x",""))
+            smm1data[0xCE29C:0xCE2A0] = speed_float
+            with open(main_binary,'wb') as smm1binary:
+                smm1binary.write(smm1data)
+                print('Successfully Patched ' + str(speed_float) + ' to 0x20A80!')
